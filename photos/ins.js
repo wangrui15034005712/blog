17dc8efe1129b40a256cd570264e84f568eed1d5
王瑞 <15034005712@163com>
2019-04-25 17:27:56 +0800

    Site updated: 2019-04-25 17:27:55

--- photos/ins.js
+++ photos/ins.js
@@ -121,9 +121,11 @@
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
-			
+      
           liTmpl += '<div style="width:30%; height:auto; float:left; padding:10px;">\
+                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
                   <img data-type="' + type + '" data-src="' + minSrc + '" src="/blog/photos/../assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                </a>\
                 <figcaption itemprop="caption description"><a onclick="imgshow(this)" href="'+src+'" target="_blank">' + data.text[i] + '</a></figcaption>\
             </div>';
         }
